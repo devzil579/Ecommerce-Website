b9ee30f2d39f652c12cf2ece188d79b8cb1943e0
Nasir Zilan <devzil589@gmail.com>
2023-08-18 15:19:14 +0600

    3rd commit

--- src/nav.js
+++ src/nav.js
@@ -3,12 +3,14 @@ import { FaTruckMoving } from 'react-icons/fa'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
 import { BsBagCheck } from 'react-icons/bs'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
 import './nav.css'
 
 const Nav = () => {
     return(
         <>
-        <div className='free'>
+        <div className='free d-flex flex-row bd-highlight mb-3 border'>
             <div className='icon'>
             <FaTruckMoving /> 
             </div>
@@ -16,22 +18,44 @@ const Nav = () => {
         </div>
         <div className='main_header'>
             <div className='container'>
-                <div className='logo'>
-                    <img src='../public/img.svg' alt='logo'></img>
+                <div className='logo d-flex justify-content-start'>
+                    <img src='./public/img.png' alt='logo'></img>
                 </div>
-                <div className='search_box'>
-                    <input placeholder='enter the product name' type='text' value='' autoComplete='off' />
-                    <button>Search</button>
+                <div className=' search_box input-group mb-3 w-50 p-3 d-flex justify-content-start'>
+                    <input className='form-control border border-info' placeholder='Search your product...' type='text' value='' autoComplete='off' />
+                    <button className='btn btn-light border border-info'>Search</button>
                 </div>
-                <div className='icon'>
-                    <div className='account'>
-                        <div className='user_icon'>
+                <div className='icon row align-items-center d-flex justify-content-between'>
+                    <div className='account d-flex flex-row bd-highlight mb-3 col'>
+                        <div className='user_icon p-2 bd-highlight'>
                         <AiOutlineUser />
                         </div>
-                        <p>Hello, user</p>
+                        <p className="p-2 bd-highlight">Hello, user</p>
                     </div>
-                    <AiOutlineHeart />
+                    <div className='second_icon col d-flex justify-content-between'>
+                    <Link to='/' className=""><AiOutlineHeart/></Link>
+                    <Link to='/' className=""><BsBagCheck/></Link>
+                    </div>
+                    
                 </div>
+            </div>
+        </div>
+        <div className='header'>
+            <div className='contact'>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/product'>Product</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
             </div>
         </div>
         </>
