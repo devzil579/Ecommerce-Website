30927614cd17b3e626f6b215237b21bc4b6b6110
Nasir Zilan <devzil589@gmail.com>
2023-08-18 12:46:56 +0600

    2nd commit

--- src/nav.js
+++ src/nav.js
@@ -1,11 +1,38 @@
 import React from 'react'
-import {FaTruckMoving} from 'react-icons/fa'
+import { FaTruckMoving } from 'react-icons/fa'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsBagCheck } from 'react-icons/bs'
 import './nav.css'
+
 const Nav = () => {
     return(
         <>
         <div className='free'>
-            <p><FaTruckMoving /> Free shipping when shopping upto $1000</p>
+            <div className='icon'>
+            <FaTruckMoving /> 
+            </div>
+            <p>Free shipping when shopping upto $1000</p>
+        </div>
+        <div className='main_header'>
+            <div className='container'>
+                <div className='logo'>
+                    <img src='../public/img.svg' alt='logo'></img>
+                </div>
+                <div className='search_box'>
+                    <input placeholder='enter the product name' type='text' value='' autoComplete='off' />
+                    <button>Search</button>
+                </div>
+                <div className='icon'>
+                    <div className='account'>
+                        <div className='user_icon'>
+                        <AiOutlineUser />
+                        </div>
+                        <p>Hello, user</p>
+                    </div>
+                    <AiOutlineHeart />
+                </div>
+            </div>
         </div>
         </>
     )
