7a910e90cbbf2455bf2090f64bcb0d2b60780d2c
Nasir Zilan <devzil589@gmail.com>
2023-08-18 12:22:20 +0600

    1st commit

---
+++ src/nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FaTruckMoving} from 'react-icons/fa'
+import './nav.css'
+const Nav = () => {
+    return(
+        <>
+        <div className='free'>
+            <p><FaTruckMoving /> Free shipping when shopping upto $1000</p>
+        </div>
+        </>
+    )
+}
+
+export default Nav
